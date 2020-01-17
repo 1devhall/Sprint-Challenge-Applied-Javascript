9acd867fc900f7cf2b1a2eecdc439ffb8dcee7ef
1devhall <devoughnh@gmail.com>
2020-01-17 14:14:33 -0500

    done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,44 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const articleInfo = document.querySelector('.cards-container')
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    console.log(response);
+    const artCont = response.data.articles
+        for( let info in artCont) {
+            artCont[info].forEach(article => articleInfo.append(articleCards(article)))
+        }
+    })
+    .catch(error => {
+        console.log("Data did not return", error);
+    });
+
+    function articleCards (article) {
+        const aCard = document.createElement('div');
+        const aHeadline = document.createElement('div');
+        const aAuthor = document.createElement('div');
+        const imgC = document.createElement('div');
+        const aImg = document.createElement('img');
+        const aAName = document.createElement('span');
+
+        aCard.append(aHeadline);
+        aCard.append(aAuthor);
+        aCard.append(imgC);
+        imgC.append(aImg);
+        aCard.append(aAName);
+
+        aCard.classList.add('card');
+        aHeadline.classList.add('headline');
+        aAuthor.classList.add('author');
+        imgC.classList.add('img-container');
+
+        aHeadline.textContent = article.headline
+        aImg.src = article.authorPhoto
+        aAName.textContent = article.authorName
+
+        return aCard
+
+    }
--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,30 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+const headerItems = document.querySelector('.header-container');
 
-function Header() {}
+function Header() {
+
+const heade = document.createElement('div');
+const aDate = document.createElement('span');
+const hTitle = document.createElement('h1');
+const temprature = document.createElement('span');
+
+heade.append(aDate);
+heade.append(hTitle);
+heade.append(temprature);
+
+heade.classList.add('header');
+aDate.classList.add('date');
+temprature.classList.add('temp');
+
+aDate.textContent = "SMARCH 28, 2019"
+hTitle.textContent = "Lambda Times"
+temprature.textContent = "98°"
+
+return heade
+
+}
+
+
+headerItems.append(Header());
