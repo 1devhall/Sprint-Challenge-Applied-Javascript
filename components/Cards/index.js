88293bdf1f6778a9d1552a18c571b766bc3ea9a7
1devhall <devoughnh@gmail.com>
2020-02-14 14:13:53 -0500

    finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const articleInfo = document.querySelector('.cards-container')
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then(response => {
+    console.log(response);
+    const artCont = response.data.articles
+        for( let info in artCont) {
+            artCont[info].forEach(article => articleInfo.append(articleCards(article)))
+        }
+    })
+    .catch(error => {
+        console.log("Data did not return", error);
+    });
+
+    function articleCards (article) {
+        const card = document.createElement('div');
+        const headline = document.createElement('div');
+        const author = document.createElement('div');
+        const imgC = document.createElement('div');
+        const aImg = document.createElement('img');
+        const name = document.createElement('span');
+
+        card.append(headline, author, imgC, name);
+        imgC.append(aImg);
+        
+
+        card.classList.add('card');
+        headline.classList.add('headline');
+        author.classList.add('author');
+        imgC.classList.add('img-container');
+
+        headline.textContent = article.headline
+        aImg.src = article.authorPhoto
+        name.textContent = article.authorName
+
+        return card
+
+    }
